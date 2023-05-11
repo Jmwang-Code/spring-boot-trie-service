407f4f89623f7fe1296d145329180addd7e1a999
王佳 <792836909@qq.com>
2023-05-11 14:44:17 +0800

    前缀树1.0.0 FEIGN

--- bin/rpc.sh
+++ bin/rpc.sh
@@ -24,13 +24,13 @@ cd "${BASE_DIR}"
 
 CLASS_PATH="${BASE_DIR}/lib/*"
 
-START_CLASS="com.cn.jmw.RegressionSearchTree"
+START_CLASS="com.cn.jmw.RPC"
 
-#java -server -Xms2G -Xmx2G  -Dspring.profiles.active=config -Dfile.encoding=UTF-8 -cp "${CLASS_PATH}" com.cn.jmw.RegressionSearchTree
+#java -server -Xms2G -Xmx2G  -Dspring.profiles.active=config -Dfile.encoding=UTF-8 -cp "${CLASS_PATH}" com.cn.jmw.RPC
 
 datart_status(){
-    #result=`ps -ef | awk '/RegressionSearchTree/ && !/awk/{print $2}' | wc -l`
-    result=`ps -ef | grep -v grep | grep "${BASE_DIR}/lib" | grep 'RegressionSearchTree' | awk {'print $2'} | wc -l`
+    #result=`ps -ef | awk '/RPC/ && !/awk/{print $2}' | wc -l`
+    result=`ps -ef | grep -v grep | grep "${BASE_DIR}/lib" | grep 'RPC' | awk {'print $2'} | wc -l`
 
     if [[ $result -eq 0 ]]; then
         return 0
@@ -48,8 +48,8 @@ trie_start(){
 
     else
         echo ""
-        #PID=`ps -ef | awk '/RegressionSearchTree/ && !/awk/{print $2}'`
-        PID=`ps -ef | grep -v grep | grep "${BASE_DIR}/lib" | grep 'RegressionSearchTree' | awk {'print $2'}`
+        #PID=`ps -ef | awk '/RPC/ && !/awk/{print $2}'`
+        PID=`ps -ef | grep -v grep | grep "${BASE_DIR}/lib" | grep 'RPC' | awk {'print $2'}`
         echo "Datart is Running Now..... PID is ${PID} "
     fi
 }
@@ -63,7 +63,7 @@ trie_stop(){
         echo ""
     else
          #ps -ef | awk '/DatartServerApplication/ && !/awk/{print $2}'| xargs kill -9
-         ps -ef | grep -v grep | grep "$BASE_DIR/lib" | grep 'RegressionSearchTree' | awk {'print $2'} | xargs kill -9
+         ps -ef | grep -v grep | grep "$BASE_DIR/lib" | grep 'RPC' | awk {'print $2'} | xargs kill -9
 
     fi
 }
@@ -72,7 +72,7 @@ trie_stop(){
 case $1 in
     start )
         echo ""
-        echo "RegressionSearchTree Starting........... "
+        echo "RPC Starting........... "
         echo ""
         trie_start
     ;;
@@ -80,18 +80,18 @@ case $1 in
     stop )
         echo ""
 
-        echo "RegressionSearchTree Stopping.......... "
+        echo "RPC Stopping.......... "
 
         echo ""
         trie_stop
     ;;
 
     restart )
-        echo "RegressionSearchTree is Restarting.......... "
+        echo "RPC is Restarting.......... "
         trie_stop
         echo ""
         trie_start
-        echo "RegressionSearchTree is Starting.......... "
+        echo "RPC is Starting.......... "
 
     ;;
 
@@ -99,13 +99,13 @@ case $1 in
         datart_status>/dev/null 2>&1
         if [[ $? -eq 0 ]]; then
             echo ""
-            echo "RegressionSearchTree is not Running......"
+            echo "RPC is not Running......"
             echo ""
         else
             echo ""
-            #PID=`ps -ef | awk '/RegressionSearchTree/ && !/awk/{print $2}'`
-            PID=`ps -ef | grep -v grep | grep "${BASE_DIR}/lib" | grep 'RegressionSearchTree' | awk {'print $2'}`
-            echo "RegressionSearchTree is Running..... PID is ${PID}"
+            #PID=`ps -ef | awk '/RPC/ && !/awk/{print $2}'`
+            PID=`ps -ef | grep -v grep | grep "${BASE_DIR}/lib" | grep 'RPC' | awk {'print $2'}`
+            echo "RPC is Running..... PID is ${PID}"
             echo ""
         fi
      ;;
